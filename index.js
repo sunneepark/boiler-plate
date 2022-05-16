71955f097d3576c0df98d135be1a0634f344d87f
sunneepark <hl2ozs@naver.com>
2022-05-17 02:02:40 +0900

    feat : 로그인, 토큰, 암호화 기능 추가

--- index.js
+++ index.js
@@ -2,15 +2,78 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key')
+const cookieParser = require('cookie-parser')
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sunny:sunny@boilerplate.ztefl.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(config.mongoURI
 ).then(() => console.log('MongoDB connected...'))
  .catch(err => console.log(err))
 
+const { User } = require('./models/User');
+const bodyParser = require('body-parser'); //body 데이터를 parse 해서 req.body 로 출력
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World~!')
 })
 
+app.post('/register', (req, res) => {
+
+  //회원 가입할 때 필요한 정보들을 client 로부터 받고 db 에 넣어줌
+
+  const user = new User(req.body)
+
+  
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
+
+app.post('/login', (req, res) => {
+  //1. 요청된 이메일 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    
+    //2. 이메일이 있다면 비밀번호 같은지 확인
+    user.comparePassword(req.body.password , (err, isMatch) => {
+      if(!isMatch) return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다"
+      })
+      console.log("ismatch", isMatch)
+      //3. 비밀번호 까지 맞다면 token 생성하기
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        // token을 저장. 쿠키, 로컬스토리지 등등
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: user._id
+        })
+
+      })
+    })
+  })
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10; //salt 를 이용해서 비밀번호 암호화
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+const jwt = require('jsonwebtoken')
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -31,6 +38,54 @@ const userSchema = mongoose.Schema({
     }
 })
 
+// 동작이 끝나면 next 로 넘어감
+userSchema.pre('save', function(next){
+
+    //비밀번호 암호화
+    //https://www.npmjs.com/package/bcrypt
+    
+    var user = this;
+    if(user.isModified('password')) { //비밀번호가 변경될 때만
+        
+        // 1. salt 생성 -> 2. salt 로 hash 값 만들기
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash(암호화된 비밀번호) in your password DB.
+                if(err) return next(err)
+                user.password = hash //hash로 비밀번호를 바꿔줌
+                next()
+            });
+        });
+
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+    
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return callback(err)
+        callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(callback) {
+    //jsonwebtoken 이용해서 token 생성
+    //https://www.npmjs.com/package/jsonwebtoken
+
+    var user = this;
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user){
+        if(err) return callback(err)
+        callback(null, user)
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
-module.exports = {User} //다른 파일에서도 사용할 수 있게 만듬
+module.exports = { User } //다른 파일에서도 사용할 수 있게 만듬
