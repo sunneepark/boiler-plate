23f19707e07db6849ec9ef7447a6edcbde690e9b
USER <user@AD01718997.local>
2022-05-17 00:27:22 +0900

    feat : nodejs + expressjs + mongo
    mongo DB cloud 연결

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://sunny:sunny@boilerplate.ztefl.mongodb.net/?retryWrites=true&w=majority'
+).then(() => console.log('MongoDB connected...'))
+ .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true, //스페이스를 없애주는
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User} //다른 파일에서도 사용할 수 있게 만듬
